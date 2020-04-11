312514372278e312a2b27379afaf397b43670aa6
Stanley Lin <kystanleylin@Stanleys-MacBook-Pro.local>
2020-04-10 22:38:44 -0400

    added stackplot

--- python/COVID-19 Test Positive Rate.py
+++ python/COVID-19 Test Positive Rate.py
@@ -1,13 +1,13 @@
 
 # coding: utf-8
 
-# In[13]:
+# In[29]:
 
 
 import numpy as np
 import pandas as pd
 import matplotlib.dates as mdates
-import datetime
+import datetime as dt
 from matplotlib.dates import MO, TU, WE, TH, FR, SA, SU
 import plotly.graph_objects as go
 
@@ -20,9 +20,23 @@ import seaborn as sns
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 
+# In[2]:
+
+
+#git status
+#git add .
+#git commit -m "comment"
+#git push
+
+#git pull
+#:q (vim's quit)
+#:w (vim's save)
+#:wq (save & quit)
+
+
 # # Load Data
 
-# In[170]:
+# In[21]:
 
 
 df= pd.DataFrame()
@@ -92,9 +106,124 @@ def plot_by_state(df, action_index):
     fig.show()
 
 
-# # Bass Model Prediction for Cases by State
+# # Test Positive Rate Trend by State
 
-# In[215]:
+# In[7]:
+
+
+def state_trend(df, state, plot= 'pos_rate'):
+    df_state= slice_state(df, state)
+    if plot == 'pos_rate':
+        plot_test_pos_rate(df_state, state)
+    if plot == 'case':
+        stackplot_state_case(df_state, state)
+
+
+# In[70]:
+
+
+def slice_state(df, state):
+    # Return a slice of df that is related to state
+    # Parameter df: raw data from api
+    # Precondition: df is a dataframe and contains the state data and all the columns from the raw data
+    # Parameter state: state abbreviation (str)
+    
+    df_state= df.loc[df['state'] == state].copy()
+    df_state['total_pos_rate']= df_state['positive']/df_state['total']*100
+    df_state['daily_pos_rate']= df_state['positiveIncrease']/df_state['totalTestResultsIncrease']*100
+    df_state['date']= pd.to_datetime(df_state['dateChecked'].str.slice(0, 10, 1), format= '%Y-%m-%d')
+    #dates= df_state['dateChecked'].str.slice(0,10,1)
+    #df_state['date']= [dt.datetime.strptime(d, '%Y-%m-%d').date() for d in dates]
+    df_state[['positive', 'death', 'recovered']]= df_state[['positive', 'death', 'recovered']].fillna(0)
+    df_state['active']= df_state['positive'] - df_state['death'] - df_state['recovered']
+    df_state.reset_index(inplace= True)
+    
+    return df_state
+
+
+# In[93]:
+
+
+def stackplot_state_case(df, state):
+    sns.reset_orig()
+    date_form= mdates.DateFormatter("%m-%d")
+    markers_on= [0, 1, 2] 
+    weekday= SU
+
+    # Print recent data points    
+    print('Daily new cases for the recent 3 days')
+    for a in range(3):
+        recoveredIncrease= df.at[a, 'recovered'] - df.at[a+1, 'recovered']
+        print('%s new positive cases: %d, new deaths: %d, new recovered: %d' % (
+            df.at[a, 'date'].strftime('%Y-%m-%d'), df.at[a, 'positiveIncrease'], 
+            df.at[a, 'deathIncrease'], recoveredIncrease))   
+
+    sns.set(font_scale= 1.2)
+    fig0, ax0= plt.subplots(figsize= (8.4, 5))
+    df= df.iloc[::-1]
+    x= list(df['date'])
+    y1, y2, y3= df['active'], df['death'], df['recovered']
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
+    pal= [sns.xkcd_rgb['denim blue'], sns.xkcd_rgb['pale red'], sns.xkcd_rgb['medium green']]
+    plt.stackplot(x, y1, y2, y3, labels= ['Active', 'Death', 'Recovered'], colors= pal)
+    plt.legend(loc= 'upper left')   
+
+    fig0.autofmt_xdate()    
+    ax0.xaxis.set_major_formatter(date_form)
+    ax0.xaxis.set_major_locator(mdates.WeekdayLocator(byweekday= weekday)) 
+
+    plt.show()
+
+
+# In[17]:
+
+
+def plot_test_pos_rate(df, state):
+    sns.reset_orig()
+    date_form= mdates.DateFormatter("%m-%d")
+    markers_on= [0, 1, 2] 
+    weekday= SU
+    
+    # Print recent data points
+    print('Cumulative test positive rate for the recent 3 days')
+    for a in range(3):
+        print('%s: %.1f' % (df_state.at[a, 'date'].strftime('%Y-%m-%d'), df_state.at[a, 'total_pos_rate']) + '%')
+    
+    fig0, ax0= plt.subplots(figsize= (8.4, 5))
+    plt.plot(df['date'], df['total_pos_rate'], color= 'tab:blue', marker= 'o', markevery= markers_on)
+    ax0.fmt_xdata= date_form # mdates.DateFormatter('%m-%d')
+    ax0.grid()
+    ax0.set(ylabel= 'Cumulative Test Positive Rate (%)',
+           title= state + ' State COVID-19 Test Positive Rate')
+    fig0.autofmt_xdate()    
+    ax0.xaxis.set_major_formatter(date_form)
+    ax0.xaxis.set_major_locator(mdates.WeekdayLocator(byweekday= weekday))
+    
+    
+    fig1, ax2= plt.subplots(figsize= (8, 5))
+    color= 'tab:red'
+    ax2.set(xlabel= 'Date')
+    ax2.set_ylabel('Daily Test Positive Rate (%)', color= color)
+    ax2.plot(df['date'], df['daily_pos_rate'], color= color, marker= 'o', markevery= markers_on)
+    ax2.tick_params(axis= 'y', labelcolor= color)   
+
+    ax3= ax2.twinx()  
+    color= 'tab:green'
+    ax3.set_ylabel('Daily Test Volume', color= color)  
+    ax3.plot(df['date'], df['totalTestResultsIncrease'], color= color, marker= 'o', markevery= markers_on)
+    ax3.tick_params(axis='y', labelcolor= color)
+    fig1.autofmt_xdate()    
+    ax3.xaxis.set_major_formatter(date_form)
+    ax3.xaxis.set_major_locator(mdates.WeekdayLocator(byweekday= weekday)) 
+
+    fig1.tight_layout()  # Otherwise the right y-label is slightly clipped
+    plt.show()
+    
+
+
+# # Bass Model Prediction for Cases in NYS
+
+# In[57]:
 
 
 df_state= slice_state(df, 'NY')
@@ -132,7 +261,7 @@ q= varfinal[2]
 print('M (estimated cumulative cases): %0.3e, p: %0.3e, q= %0.3e' % (m, p, q))
 
 # Extend date range for prediction
-pred_days= 61
+pred_days= 41
 times = pd.date_range(df_Y.at[len(df_Y)-1, 'date'], periods= pred_days, freq= '1D')
 df_times = pd.DataFrame({'date': times[1:]})
 df_Y= df_Y.append(df_times, ignore_index = True, sort= False)
@@ -182,186 +311,28 @@ ax1.xaxis.set_major_formatter(date_form)
 ax1.xaxis.set_major_locator(mdates.WeekdayLocator(byweekday= weekday))
 
 
-# # Test Positive Rate Trend by State
-
-# In[7]:
-
-
-def state_pos_rate(df, state):
-    df_state= slice_state(df, state)
-    print('Cumulative test positive rate for the recent 3 days')
-    for a in range(3):
-        print('%s: %.1f' % (df_state.at[a, 'date'].strftime('%Y-%m-%d'), df_state.at[a, 'total_pos_rate']) + '%')
-    plot_test_pos_rate(df_state, state)
-    #plot_state_trend(df_state, state, action_index)
-
-
-# In[8]:
-
-
-def slice_state(df, state):
-    df_state= df.loc[df['state'] == state].copy()
-    df_state['total_pos_rate']= df_state['positive']/df_state['total']*100
-    df_state['daily_pos_rate']= df_state['positiveIncrease']/df_state['totalTestResultsIncrease']*100
-    df_state['date']= pd.to_datetime(df_state['dateChecked'].str.slice(0, 10, 1), format= '%Y-%m-%d')
-    df_state.reset_index(inplace= True)
-    
-    return df_state
-
-
-# In[9]:
-
-
-# incomplete
-def plot_state_trend(df, state, action_index):
-    sns.reset_orig()
-    date_form= mdates.DateFormatter("%m-%d")
-    markers_on= [0, 1, 2] 
-    weekday= SU
-    
-    # If action_index == 0, plot cumulative test positive rate and daily positive rate & daily test volume
-    # If action_index == 1, plot cumulative & new positive cases and cumulative & new deaths
-    
-    data= {'ax0_variable': ['total_pos_rate', 'positive'],
-           'ax1_variable': [np.nan, 'positiveIncrease'],
-           'ax2_variable': ['daily_pos_rate', 'death'],
-           'ax3_variable': ['totalTestResultsIncrease', 'deathIncrease'],
-           'ax0_plot_title': ['Test Positive Rate', 'Positive Cases'],
-           'ax2_plot_title': [np.nan, 'Deaths'],
-           'ax0_ylabel': ['Cumulative Test Positive Rate (%)', 'Cumulative Cases'],
-           'ax1_ylabel': [np.nan, 'New Cases'],
-           'ax2_ylabel': ['Daily Test Positive Rate (%)', 'Cumulative Deaths'],
-           'ax3_ylabel': ['Daily Test Volume', 'New Deaths']
-          }
-    action= pd.DataFrame(data, columns= ['ax0_variable', 'ax1_variable', 'ax2_variable', 'ax3_variable',
-                                         'ax0_plot_title', 'ax2_plot_title', 'ax0_ylabel', 'ax1_ylabel', 
-                                         'ax2_ylabel', 'ax3_ylabel'])
-    
-    fig0, ax0= plt.subplots(figsize=(8.4, 5))
-    plt.plot(df['date'], df[action.at[action_index, 'ax0_variable']], 
-             color= 'tab:blue', marker= 'o', markevery= markers_on)
-    ax0.fmt_xdata= date_form # mdates.DateFormatter('%m-%d')
-    ax0.set(ylabel= action.at[action_index, 'ax0_ylabel'],
-            title= state + ' State' + action.at[action_index, 'ax0_plot_title'])
-    fig0.autofmt_xdate()    
-    ax0.xaxis.set_major_formatter(date_form)
-    ax0.xaxis.set_major_locator(mdates.WeekdayLocator(byweekday= weekday)) 
-    
-    if (isinstance(action.at[action_index, 'ax1_variable'], str)):
-        ax1= ax0.twinx()  
-        color= 'tab:orange'  
-        ax1.plot(df['date'], df[action.at[action_index, 'ax1_variable']], 
-                 color= color, marker= 'o', markevery= markers_on)
-        ax1.set_ylabel(action.at[action_index, 'ax1_ylabel'], color= color)
-        ax1.tick_params(axis='y', labelcolor= color)
-        
-        color= 'tab:blue'
-        ax0.set_ylabel(color= color)
-        ax0.tick_params(axis= 'y', labelcolor= color)
-    else:
-        ax0.grid()
-    
-    fig1, ax2= plt.subplots(figsize=(8, 5))
-    color= 'tab:red'
-    ax2.set(xlabel= 'Date')
-    ax2.set_ylabel(action.at[action_index, 'ax2_ylabel'], color= color)
-    ax2.plot(df['date'], df[action.at[action_index, 'ax2_variable']],
-             color= color, marker= 'o', markevery= markers_on)
-    ax2.tick_params(axis= 'y', labelcolor= color)
-    fig1.autofmt_xdate()    
-    ax2.xaxis.set_major_formatter(date_form)
-    ax2.xaxis.set_major_locator(mdates.WeekdayLocator(byweekday= weekday))
-
-    if (isinstance(action.at[action_index, 'ax3_variable'], str)):
-        ax3= ax2.twinx()  
-        color= 'tab:green'
-        ax3.set_ylabel(action.at[action_index, 'ax3_ylabel'], color= color)  
-        ax3.plot(df['date'], df[action.at[action_index, 'ax3_variable']],
-                 color= color, marker= 'o', markevery= markers_on)
-        ax3.tick_params(axis='y', labelcolor= color)    
-        ax3.xaxis.set_major_formatter(date_form)
-        ax3.xaxis.set_major_locator(mdates.WeekdayLocator(byweekday= weekday))
-      
-    fig1.tight_layout()  # Otherwise the right y-label is slightly clipped
-    plt.show()
-
-
-# In[11]:
-
-
-def plot_test_pos_rate(df, state):
-    sns.reset_orig()
-    date_form= mdates.DateFormatter("%m-%d")
-    markers_on= [0, 1, 2] 
-    weekday= SU
-    
-    # If action_index == 0, plot cumulative test positive rate and daily positive rate & daily test volume
-    # If action_index == 1, plot cumulative & new positive cases and cumulative & new deaths
-    
-    # no use for now
-    data= {'ax0_variable': ['total_pos_rate', 'positive'],
-           'ax1_variable': [np.nan, 'positiveIncrease'],
-           'ax2_variable': ['daily_pos_rate', 'death'],
-           'ax3_variable': ['Daily Test Volume', 'deathIncrease'],
-           'ax0_plot_title': ['Test Positive Rate', 'Positive Cases'],
-           'ax2_plot_title': [np.nan, 'Deaths'],
-           'ax0_ylabel': ['Cumulative Test Positive Rate (%)', 'Cumulative Cases'],
-           'ax1_ylabel': [np.nan, 'New Cases'],
-           'ax2_ylabel': ['Daily Test Positive Rate (%)', 'Cumulative Deaths'],
-           'ax3_ylabel': ['Daily Test Volume', 'New Deaths']
-          }
-    action= pd.DataFrame(data, columns= ['ax0_variable', 'ax1_variable', 'ax2_variable', 'ax3_variable',
-                                        'ax0_plot_title', 'ax2_plot_title', 'ax0_ylabel', 'ax1_ylabel', 
-                                        'ax2_ylabel', 'ax3_ylabel'])
-    
-    fig0, ax0= plt.subplots(figsize=(8.4, 5))
-    plt.plot(df['date'], df['total_pos_rate'], color= 'tab:blue', marker= 'o', markevery= markers_on)
-    ax0.fmt_xdata= date_form # mdates.DateFormatter('%m-%d')
-    ax0.grid()
-    ax0.set(ylabel= 'Cumulative Test Positive Rate (%)',
-           title= state + ' State COVID-19 Test Positive Rate')
-    fig0.autofmt_xdate()    
-    ax0.xaxis.set_major_formatter(date_form)
-    ax0.xaxis.set_major_locator(mdates.WeekdayLocator(byweekday= weekday))
-    
-    
-    fig1, ax2= plt.subplots(figsize=(8, 5))
-    color= 'tab:red'
-    ax2.set(xlabel= 'Date')
-    ax2.set_ylabel('Daily Test Positive Rate (%)', color= color)
-    ax2.plot(df['date'], df['daily_pos_rate'], color= color, marker= 'o', markevery= markers_on)
-    ax2.tick_params(axis= 'y', labelcolor= color)   
-
-    ax3= ax2.twinx()  
-    color= 'tab:green'
-    ax3.set_ylabel('Daily Test Volume', color= color)  
-    ax3.plot(df['date'], df['totalTestResultsIncrease'], color= color, marker= 'o', markevery= markers_on)
-    ax3.tick_params(axis='y', labelcolor= color)
-    fig1.autofmt_xdate()    
-    ax3.xaxis.set_major_formatter(date_form)
-    ax3.xaxis.set_major_locator(mdates.WeekdayLocator(byweekday= weekday)) 
-
-    fig1.tight_layout()  # Otherwise the right y-label is slightly clipped
-    plt.show()
-    
-
-
 # # User Interface
 
-# In[9]:
+# In[15]:
 
 
 latest_by_state(df)
 
 
-# In[12]:
+# In[24]:
 
 
-state_pos_rate(df, 'NY')
+state_trend(df, 'NY', plot= 'pos_rate')
 
 
-# In[40]:
+# In[94]:
 
 
-state_pos_rate(df, 'CA')
+state_trend(df, 'NY', plot= 'case')
+
+
+# In[19]:
+
+
+state_trend(df, 'NJ', plot= 'pos_rate')
 
